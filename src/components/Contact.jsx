29a97a4cac684674dbe48a8d064686771c58d7c3
amalkrishna0123 <amalikka0@gmail.com>
2024-09-26 18:37:28 +0530

    Added Mananthavadi Actual Link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -168,7 +168,7 @@ const Contact = () => {
 
             <div className='w-full'>
               <div className='text-center mb-5 titleText2 text-2xl text-[#FF6C00]'>Hyper City Manathavady</div>
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3905.6933557482357!2d76.00212177487263!3d11.786627888430349!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba5dec6266381d3%3A0xe1768e7afc7da796!2s457%2C%20Calicut%20Rd%2C%20Mananthavady%2C%20Nalloornad%2C%20Kerala%20670645!5e0!3m2!1sen!2sin!4v1727326780032!5m2!1sen!2sin" width="600" height="450" style={{border:0}}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='w-full h-[500px] rounded-3xl  mb-6 px-2'></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3905.6932801950243!2d76.0053913!3d11.786633199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba5df89326eaec5%3A0x467d6b00878f6e4!2sHypercity%20hypermarket!5e0!3m2!1sen!2sin!4v1727355882058!5m2!1sen!2sin" width="600" height="450" style={{border:0}}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='w-full h-[500px] rounded-3xl  mb-6 px-2'></iframe>
           </div>
     
           <div className='w-full'>
